e56d55eefa5a1dbf8b2ef91f424d3b4a06a11e65
João Pereira <55116713+uBatatinha@users.noreply.github.com>
2024-09-04 16:14:36 -0300

    aula 3 ex8.js

--- aula 3/aula 3 ex8.js
+++ aula 3/aula 3 ex8.js
@@ -4,7 +4,7 @@ const num = parseInt(prompt('Digite um número: '));
 
 function isPrime(n) {
   if (n <= 1) {
-    return false; // 0 and 1 are not prime
+    return false;
   }
   for (let i = 2; i * i <= n; i++) {
     if (n % i === 0) {
